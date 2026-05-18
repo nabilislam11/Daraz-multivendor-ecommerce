a790b43934d35517ffb4857cd78b6966fb1da967
nabilislam11 <hamjanabil11@gmail.com>
2026-05-18 19:45:39 +0600

    auth  middleware create

---
+++ src/middleware/auth.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const protect = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  if (!token) {
+    return res.status(401).json({
+      message: "NOt authorized,no token",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(409).json({
+      message: "Not authorizedn  token failed ",
+    });
+  }
+};
+
+const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(409).json({
+        message: "You do not have permission ",
+      });
+    }
+    next();
+  };
+};
+module.exports = { protect, restrictTo };
